f271e7e5ecf306cc5d1e83ef8eebdc3afe2c0e12
markbor198 <79873427+markbor198@users.noreply.github.com>
2021-03-08 00:33:39 -0600

    updated mar 8 12:33 am

--- team 7/state0.js
+++ team 7/state0.js
@@ -12,21 +12,45 @@ first.state0.prototype = {
         game.load.image('beach', 'space1.png');
         game.load.image('star', 'coin.png');
         game.load.image('sonic','unicorn.png', 250, 300);
-        game.load.spritesheet('boss', 'boss.png');
+        game.load.image('orange', 'o_platform.png');
+        game.load.image('red', 'r_platform.png');
+        game.load.image('yellow', 'y_platform.png');
+        game.load.image('end', 'black.png');
+        game.load.spritesheet('boss', 'boss2.png', 300, 180);
     },
     create: function(){
         game.physics.startSystem(Phaser.Physics.ARCADE);
         console.log('state0');
         game.world.setBounds(0, 0, 2050, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        platforms = game.add.group();
+        
+        // enable physics for any object created in group
+        platforms.enableBody = true;
+        
         var beach = game.add.sprite(0, 0, 'beach');
+        
+        
+        boss = game.add.sprite(game.world.width*0.8, game.world.height-250, 'boss');
+        boss.anchor.setTo(0.5);
+        game.physics.enable(boss, Phaser.Physics.ARCADE);
+        boss.body.velocity.set(250, -250);
+        boss.body.collideWorldBounds = true;
+        boss.body.bounce.set(1);
+        boss.body.immovable = true;
+        boss.animations.add('fly', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]);
+        
+        
+        
+        
+        
         sonic = game.add.sprite(centerX, centerY, 'sonic');
         sonic.anchor.setTo(0.5, 0.5);
         sonic.scale.setTo(0.7, 0.7);
         game.physics.enable(sonic);
         sonic.body.collideWorldBounds = true;
         sonic.body.immovable = true;
-        boss = game.add.sprite(centerX+50, centerY +50, 'boss')
+        
         
         
         star = game.add.sprite(game.world.width*0.5, game.world.height-50, 'star');
@@ -38,6 +62,11 @@ first.state0.prototype = {
         star.body.bounce.set(1);
         star.checkWorldBounds = true;
         star.body.immovable = true;
+        
+        
+        
+        
+        
         
 
 
@@ -56,9 +85,12 @@ first.state0.prototype = {
     //  We will enable physics for any star that is created in this group
     },
     update: function(){
+        boss.animations.play('fly');
         game.physics.arcade.collide(sonic, star);
+        game.physics.arcade.collide(sonic, boss);
         
         game.physics.arcade.collide(sonic, star, collectStar);
+        game.physics.arcade.collide(sonic, boss, killPlayer);
     
         
         sonic.body.velocity.x = 0;
@@ -96,6 +128,12 @@ function collectStar(sonic, star) {
     star.kill();
     score += 10;
     scoreText.text = 'Score: ' + score;
+}
+
+function killPlayer(sonic, boss) {
+    sonic.kill();
+    game.add.sprite(0, 1110, 'end');
+    game.add.text(500, 500, 'YOU LOSE', { fontSize: '100px', fill:'#FFFFFF'});
 }
 
 function addKeyCallback(key, fn, args) {
