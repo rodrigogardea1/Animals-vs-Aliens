15639cb06efe4123dcb9abc2ffa9c10983e37c2d
markbor198 <79873427+markbor198@users.noreply.github.com>
2021-04-05 15:52:41 -0500

    NEW OFFICIAL VERSION

---
+++ js/prefabs/Enemy.js
@@ -0,0 +1,38 @@
+var Platformer = Platformer || {};
+
+Platformer.Enemy = function (game_state, position, properties) {
+    "use strict";
+    Platformer.Prefab.call(this, game_state, position, properties);
+    
+    
+    this.animations.add("walking", [16, 17, 18, 19, 20, 21, 22], 5, true);
+    this.animations.play("walking");
+    
+    this.walking_speed = +properties.walking_speed;
+    this.walking_distance = +properties.walking_distance;
+    
+    // saving previous x to keep track of walked distance
+    this.previous_x = this.x;
+    
+    this.game_state.game.physics.arcade.enable(this);
+    this.body.velocity.x = properties.direction * this.walking_speed;
+    
+    this.scale.setTo(properties.direction, 1);
+    
+    this.anchor.setTo(0.5);
+};
+
+Platformer.Enemy.prototype = Object.create(Platformer.Prefab.prototype);
+Platformer.Enemy.prototype.constructor = Platformer.Enemy;
+
+Platformer.Enemy.prototype.update = function () {
+    "use strict";
+    this.game_state.game.physics.arcade.collide(this, this.game_state.layers.collision);
+    
+    // change the direction if walked the maximum distance
+    if (Math.abs(this.x - this.previous_x) >= this.walking_distance) {
+        this.body.velocity.x *= -1;
+        this.previous_x = this.x;
+        this.scale.setTo(-this.scale.x, 1);
+    }
+};
