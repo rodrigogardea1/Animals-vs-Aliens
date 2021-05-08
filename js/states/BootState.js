7fabc5e6d3ad2fca5bc94ba3eda74d92d2dd0b75
markbor198 <79873427+markbor198@users.noreply.github.com>
2021-05-07 22:29:57 -0500

    Add files via upload

--- js/states/BootState.js
+++ js/states/BootState.js
@@ -1,24 +1,24 @@
 var Phaser = Phaser || {};
 var Platformer = Platformer || {};
- 
+
 Platformer.BootState = function () {
     "use strict";
     Phaser.State.call(this);
 };
- 
+
 Platformer.prototype = Object.create(Phaser.State.prototype);
 Platformer.prototype.constructor = Platformer.BootState;
- 
+
 Platformer.BootState.prototype.init = function (level_file) {
     "use strict";
     this.level_file = level_file;
 };
- 
+
 Platformer.BootState.prototype.preload = function () {
     "use strict";
     this.load.text("level1", this.level_file);
 };
- 
+
 Platformer.BootState.prototype.create = function () {
     "use strict";
     var level_text, level_data;
--- js/states/TiledState.js
+++ js/states/TiledState.js
@@ -4,6 +4,20 @@ var Platformer = Platformer || {};
 Platformer.TiledState = function () {
     "use strict";
     Phaser.State.call(this);
+    
+    this.prefab_classes = {
+        "player": Platformer.Player.prototype.constructor,
+        "ground_enemy": Platformer.GroundEnemy.prototype.constructor,
+        "flying_enemy": Platformer.FlyingEnemy.prototype.constructor,
+        "running_enemy": Platformer.RunningEnemy.prototype.constructor,
+        "goal": Platformer.Goal.prototype.constructor,
+        "checkpoint": Platformer.Checkpoint.prototype.constructor,
+        "coin": Platformer.Coin.prototype.constructor,
+        "score": Platformer.Score.prototype.constructor,
+        "lives": Platformer.Lives.prototype.constructor,
+        "life_item": Platformer.LifeItem.prototype.constructor,
+        "fireball_item": Platformer.FireballItem.prototype.constructor
+    };
 };
 
 Platformer.TiledState.prototype = Object.create(Phaser.State.prototype);
@@ -11,6 +25,7 @@ Platformer.TiledState.prototype.constructor = Platformer.TiledState;
 
 Platformer.TiledState.prototype.init = function (level_data) {
     "use strict";
+    var tileset_index;
     this.level_data = level_data;
     
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -23,13 +38,17 @@ Platformer.TiledState.prototype.init = function (level_data) {
     
     // create map and set tileset
     this.map = this.game.add.tilemap(level_data.map.key);
-    this.map.addTilesetImage(this.map.tilesets[0].name, level_data.map.tileset);
+    tileset_index = 0;
+    this.map.tilesets.forEach(function (tileset) {
+        this.map.addTilesetImage(tileset.name, level_data.map.tilesets[tileset_index]);
+        tileset_index += 1;
+    }, this);
 };
 
 Platformer.TiledState.prototype.create = function () {
     "use strict";
     var group_name, object_layer, collision_tiles;
-    this.map.addTilesetImage("background");
+    
     // create map layers
     this.layers = {};
     this.map.layers.forEach(function (layer) {
@@ -64,32 +83,52 @@ Platformer.TiledState.prototype.create = function () {
             this.map.objects[object_layer].forEach(this.create_object, this);
         }
     }
+    
+    this.init_hud();
+    
+    this.game.camera.follow(this.prefabs.player);
 };
 
 Platformer.TiledState.prototype.create_object = function (object) {
     "use strict";
-    var position, prefab;
+    var object_y, position, prefab;
     // tiled coordinates starts in the bottom left corner
-    position = {"x": object.x + (this.map.tileHeight / 2), "y": object.y - (this.map.tileHeight / 2)};
+    object_y = (object.gid) ? object.y - (this.map.tileHeight / 2) : object.y + (object.height / 2);
+    position = {"x": object.x + (this.map.tileHeight / 2), "y": object_y};
     // create object according to its type
-    switch (object.type) {
-    case "player":
-        prefab = new Platformer.Player(this, position, object.properties);
-        break;
-    case "ground_enemy":
-        prefab = new Platformer.Enemy(this, position, object.properties);
-        break;
-    case "flying_enemy":
-        prefab = new Platformer.FlyingEnemy(this, position, object.properties);
-        break;
-    case "goal":
-        prefab = new Platformer.Goal(this, position, object.properties);
-        break;
+    if (this.prefab_classes.hasOwnProperty(object.type)) {
+        prefab = new this.prefab_classes[object.type](this, position, object.properties);
     }
     this.prefabs[object.name] = prefab;
 };
 
 Platformer.TiledState.prototype.restart_level = function () {
     "use strict";
-    this.game.state.restart(true, false, this.level_data);
+	// restart the game only if the checkpoint was not reached
+    if (this.prefabs.checkpoint && this.prefabs.checkpoint.checkpoint_reached) {
+        this.prefabs.player.x = this.prefabs.checkpoint.x;
+        this.prefabs.player.y = this.prefabs.checkpoint.y;
+    } else {
+        localStorage.player_lives = this.prefabs.player.lives;
+        localStorage.player_score = this.prefabs.player.score;
+        this.game.state.restart(true, false, this.level_data);
+    }
+};
+
+Platformer.TiledState.prototype.game_over = function () {
+    "use strict";
+    localStorage.clear();
+    this.game.state.start("BootState", true, false, "assets/levels/level1.json");
+};
+
+Platformer.TiledState.prototype.init_hud = function () {
+    "use strict";
+    var score_position, score, lives_position, lives;
+    score_position = new Phaser.Point(100, 20);
+    score = new Platformer.Score(this, score_position, {"text": "Score: 0", "group": "hud"});
+    this.prefabs['score'] = score;
+    
+    lives_position = new Phaser.Point(20, 20);
+    lives = new Platformer.Lives(this, lives_position, {"texture": "player_spritesheet", "group": "hud", "frame": 3, "spacing": 16});
+    this.prefabs["lives"] = lives;
 };
