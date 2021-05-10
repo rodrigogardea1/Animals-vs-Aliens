478e15d286a414692750b9737d1fd3186b42a23e
markbor198 <79873427+markbor198@users.noreply.github.com>
2021-05-10 13:47:06 -0500

    Add files via upload

---
+++ js/states/state1.js
@@ -0,0 +1,23 @@
+demo.state1 = function(){};
+demo.state1.prototype = {
+    
+    preload: function () {
+        this.game.load.image('gameover', 'assets/images/gameover.png')
+       
+    },
+    create: function () {
+
+        var gameoverLabel = stateText = game.add.image(700, 250, 'gameover');
+        stateText.anchor.setTo(1.1, 0.2);
+
+    },
+
+    update: function () {
+        stateText.text = " GAME OVER \n Click to restart";
+        stateText.visible = true;
+
+            //the "click to restart" handler
+        game.input.onTap.addOnce(function () {
+        game.state.start('BootState');});
+}
+};
